fb2f07730abc5d4545c95afd1624cd059e445abf
kristi <tanellari@gmail.com>
2022-11-28 10:47:58 -0500

    removed laravel default welcome page from routes

--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/charts', function () {
-
     return view('charts');
 });
