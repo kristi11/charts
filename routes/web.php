88cd122c11def4ec311609570839da70c5b6a185
kristi <tanellari@gmail.com>
2022-12-01 12:13:36 -0500

    add content editing ability

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Models\Announcement;
 use App\Models\Chart;
 use App\Models\User;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,7 +16,6 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
 Route::get('/charts', function () {
     return view('charts');
 });
@@ -24,8 +25,45 @@ Route::get('/', function () {
 });
 
 Route::get('/stats', function () {
-    $revenue = Chart::where('created_at', '>=' , now()->subDays(30))->sum('total');
-    return view('stats', [
-        'revenue' => $revenue,
+    return view('stats');
+});
+
+Route::get('/announcement', function () {
+
+    $announcement = Announcement::first();
+
+    @abort_if(!$announcement->isActive, 404);
+
+    return view('announcement',[
+        'announcement' => $announcement,
     ]);
+});
+
+Route::get('/announcement/edit', function () {
+
+    $announcement = Announcement::first();
+
+    return view('edit-announcement',[
+        'announcement' => $announcement,
+    ]);
+});
+
+Route::patch('/announcement/update', function (Request $request) {
+    $fields = $request->validate([
+        'isActive' => 'required',
+        'bannerText' => 'required',
+        'bannerColor' => 'required',
+        'titleText' => 'required',
+        'titleColor' => 'required',
+        'content' => 'required',
+        'buttonText' => 'required',
+        'buttonColor' => 'required',
+        'buttonLink' => 'required|url',
+    ]);
+
+    $announcement = Announcement::first();
+
+    $announcement->update($fields);
+
+    return back()->with('success_message', 'Announcement updated');
 });
